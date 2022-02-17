cb50ff1bea9db2418c44b446e8469a203d57c4e4
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-02-17 17:17:16 -0300

    Creadas las rutas básicas de la página

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}/>
+          <Route path="/*" element={<p>No se encontró nada en este lugar.</p>}/>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
