21c8eba99f99c7c1f21e52fa9a056317a9549b33
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-02-18 14:51:19 -0300

    PR Fixes: Componentes de rutas creadas y pequeños ajustes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}/>
-          <Route path="/*" element={<p>No se encontró nada en este lugar.</p>}/>
-        </Routes>
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
