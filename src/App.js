89f9897c0fba3af4828bc6d2768a838b78823f8f
ema <lotofacundo6@gmail.com>
2022-02-18 13:18:03 -0300

    OT139-19 setup-loader-fix-code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import  Loader  from './components/loader/loader';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Loader/>
-    </div>
+ return(
+   <div className="App">
+
+   </div>
   );
 }
 
