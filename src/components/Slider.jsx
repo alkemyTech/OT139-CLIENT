6b50862052b10096e92445e878e77d9fc85782d8
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-19 09:52:24 -0300

    PR review OT139-28-Componente-Slider: comments fix

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,17 +4,16 @@ import { Carousel } from "react-bootstrap";
 export default function Slider(props) {
   return (
     <Carousel>
-      {props.data &&
-        props.data.map((item, index) => {
-          return (
-            <Carousel.Item key={index}>
-              <img className="d-block" src={item.imageUrl} alt={item.text} />
-              <Carousel.Caption>
-                <p>{item.text}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          );
-        })}
+      {props?.data?.map((item, index) => {
+        return (
+          <Carousel.Item key={index}>
+            <img className="d-block" src={item.imageUrl} alt={item.text} />
+            <Carousel.Caption>
+              <p>{item.text}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 }
