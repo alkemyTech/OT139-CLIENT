69a17dacf031eafc73c34dc4d84923855aa3803b
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-19 09:47:34 -0300

    PR OT139-20-Setup-Alertas  review: comment fix

--- src/components/Alert/Index.jsx
+++ src/components/Alert/Index.jsx
@@ -1,10 +1,13 @@
 import Swal from "sweetalert2";
+
 export function InfoAlert(title, message) {
   return Swal.fire(title, message, "info");
 }
+
 export function ErrorAlert(title, message) {
   return Swal.fire(title, message, "error");
 }
+
 export function SuccessAlert(title, message) {
   return Swal.fire(title, message, "success");
 }
