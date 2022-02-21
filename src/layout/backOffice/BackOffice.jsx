beca8a71aca3c8f4164199dad3832eab4b921e8c
AndresHense <andreshense@gmail.com>
2022-02-21 11:15:30 -0300

    add backoffice layout

---
+++ src/layout/backOffice/BackOffice.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+//Mock header
+const Header=()=>{
+    return <>header</>
+}
+
+//Mock footer
+const Footer=()=>{
+    return <>footer</>
+}
+
+const BackOffice = ({children}) => {
+  return (
+   <>
+    <Header/>
+    {children}
+    <Footer/>
+   </>
+  )
+}
+
+export default BackOffice
