2d6d2a0045cdd20bace309c1a82dc80436cc090f
tamiisoledad <tamiisoledad4@gmail.com>
2022-02-21 09:33:53 -0300

    comentarios borrados

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -73,78 +73,8 @@ const LoginForm = (props) => {
                 </Form>
 
             </Formik>
-            {/*  <Form  onSubmit={handleSubmit}  className={styles.form}>
-
-                <Form.Group className={styles.group} controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Field 
-                        className={styles.input}
-                        type="email"
-                        name="email" 
-                        placeholder="Introduzca su Email" />
-                       {<ErrorMessage name="email"/> && <ErrorMessage name="email">
-                           {message => <small className={styles.error}>{message}</small>}
-                       </ErrorMessage>}
-                </Form.Group>
-
-                <Form.Group className={styles.group} controlId="formBasicPassword">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Field 
-                        className={styles.input}
-                        type="password"
-                        name="password"
-                        placeholder="Introduzca su contraseña" />
-                        {<ErrorMessage name="password"/> && <ErrorMessage name="password">
-                           {message => <small className={styles.error}>{message}</small>}
-                       </ErrorMessage>}
-                        
-                </Form.Group>
-
-                <Button 
-                     disabled={isSubmitting || !isValid} 
-                    variant="primary" 
-                    type="submit"
-                    >
-                    Iniciar sesión
-                </Button>
-
-            </Form> */}
         </div>
     );
 }
 
-export default LoginForm /* withFormik({
-    mapPropsToValues(props){
-        return{
-            email: '',
-            password: ''
-        }
-    },
-
-    validate(values){
-        let regExEmail =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
-        const errors = {};
-
-            if(!values.email){
-                errors.email = "El email es requerido";
-            } else if(!regExEmail.test(values.email)){
-                errors.email = "Debes introducir un email valido";
-            }
-            if(!values.password){
-                errors.password = "La contraseña es requerida";
-            }else if(values.password.length < 6){
-                errors.password = "La contraseña debe tener más de 6 caracteres";
-            }
-        
-        return errors;
-    },
-
-    handleSubmit(values, formikBag){
-        let usuario = {
-            email: values.email,
-            password: values.password
-        }
-        formikBag.setSubmitting(false);
-        console.log(usuario);
-    }
-})(LoginForm); */
+export default LoginForm;
