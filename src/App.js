a184356aa2172b592bdfeabe7b190151d0b4b1b9
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-02-22 00:39:36 -0300

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import FormEditHome from './Components/EditHome/FormEditHome'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <FormEditHome/>
     </div>
   );
 }
