46c329a6690be79b158407cdbeaac49f3876ff2c
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-23 07:35:30 -0300

    PR review OT139-20: Cambios en alerta de confirmacion (comments fix)

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -11,3 +11,34 @@ export function ErrorAlert(title, message) {
 export function SuccessAlert(title, message) {
   return Swal.fire(title, message, "success");
 }
+
+export function ConfirmAlert(title, message) {
+  return Swal.fire({
+    title: title,
+    text: message,
+    icon: "warning",
+    showDenyButton: true,
+    confirmButtonText: "Confirmar",
+    denyButtonText: `Cancelar`,
+    confirmButtonColor: "#78D052",
+    cancelButtonColor: "#d33",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Confirmado",
+        text: "",
+        icon: "success",
+        confirmButtonColor: "#78D052",
+      });
+      return { confirmacion: true };
+    } else if (result.isDenied) {
+      Swal.fire({
+        title: "Cancelado",
+        text: "",
+        icon: "info",
+        confirmButtonColor: "#78D052",
+      });
+      return { confirmacion: false };
+    }
+  });
+}
