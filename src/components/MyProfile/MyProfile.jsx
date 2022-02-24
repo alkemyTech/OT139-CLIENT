d996d40d914be3fa32c01fcb30968156a25ed194
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-02-23 23:41:38 -0300

    PR Fixes: Recreacion del mockup del componente MyProfile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
@@ -6,28 +6,30 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import './myProfile.css';
 
-function getProfileData() {
-  // @TODO Implementar una vez que la API esté lista
-  // Llamar a un endpoint de Autenticación con los datos del usuario
-  return {
-    firstName: 'John',
-    lastName: 'Smith',
-    email: 'johnsmith@example.com',
-  };
-}
-
 export default function MyProfile() {
-  const profileData = getProfileData();
+  const [profileData, setProfileData] = useState();
 
   const handleEditProfile = () => {
     // @TODO Implementar la funcionalidad del botón Editar Cuenta
-    console.log('Se está editando el perfil');
-  }
+  };
 
   const handleDeleteAccount = () => {
     // @TODO Implementar la funcionalidad del botón Eliminar Cuenta
-    console.log('Se está eliminando la cuenta');
-  }
+  };
+
+  useEffect(() => {
+    async function getProfileData() {
+      // @TODO Implementar una vez que la API esté lista
+      // Llamar a un endpoint de Autenticación con los datos del usuario
+      setProfileData({
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'johnsmith@example.com',
+      });
+    }
+
+    getProfileData();
+  }, []);
 
   return (
     <div className='max_width_wrapper_medium mx-auto'>
@@ -35,64 +37,67 @@ export default function MyProfile() {
         <Card.Body className='px-md-5'>
           <Row className='mb-3'>
             <Col>
-              <div className='title_large'>
-                Mi Perfil
-              </div>
+              <div className='title_large'>Mi Perfil</div>
             </Col>
           </Row>
-          <Row className='mb-4'>
-            <Col>
-              <div className='subtitle'>
-                Datos de Cuenta
-              </div>
-            </Col>
-          </Row>
-          <Row className='mb-4'>
-            <Col className='disableText' sm="6">
-              Nombre
-            </Col>
-            <Col className='text_strong' sm="6">
-              {profileData.firstName}
-            </Col>
-          </Row>
-          <Row className='mb-4'>
-            <Col className='disableText' sm="6">
-              Apellido
-            </Col>
-            <Col className='text_strong' sm="6">
-              {profileData.lastName}
-            </Col>
-          </Row>
-          <Row className='mb-4'>
-            <Col className='disableText' sm="6">
-              E-Mail
-            </Col>
-            <Col className='text_strong' sm="6">
-              {profileData.email}
-            </Col>
-          </Row>
+          {!profileData ? (
+            'Cargando...'
+          ) : (
+            <>
+              <Row className='mb-4'>
+                <Col>
+                  <div className='subtitle'>Datos de Cuenta</div>
+                </Col>
+              </Row>
+              <Row className='mb-4'>
+                <Col className='disableText' sm='6'>
+                  Nombre
+                </Col>
+                <Col className='text_strong' sm='6'>
+                  {profileData.firstName}
+                </Col>
+              </Row>
+              <Row className='mb-4'>
+                <Col className='disableText' sm='6'>
+                  Apellido
+                </Col>
+                <Col className='text_strong' sm='6'>
+                  {profileData.lastName}
+                </Col>
+              </Row>
+              <Row className='mb-4'>
+                <Col className='disableText' sm='6'>
+                  E-Mail
+                </Col>
+                <Col className='text_strong' sm='6'>
+                  {profileData.email}
+                </Col>
+              </Row>
+            </>
+          )}
           <Row className='mb-5'>
             <Col>
-              <Button size="lg" onClick={handleEditProfile}>
+              <Button disabled={!profileData} size='lg' onClick={handleEditProfile}>
                 Editar Cuenta
               </Button>
             </Col>
           </Row>
           <Row className='mb-3'>
             <Col>
-              <div className='subtitle color_danger'>
-                Eliminar Cuenta
-              </div>
+              <div className='subtitle color_danger'>Eliminar Cuenta</div>
             </Col>
           </Row>
           <Row className='mb-2'>
             <Col>
-              <p>Una vez que haya eliminado su cuenta, no hay vuelta atrás. Por favor esté seguro.</p>
+              <p>
+                Una vez que haya eliminado su cuenta, no hay vuelta atrás. Por
+                favor esté seguro.
+              </p>
             </Col>
           </Row>
           <Row className='mb-4'>
             <Col>
-              <Button size="lg" variant='danger' onClick={handleDeleteAccount}>
+              <Button size='lg' variant='danger' onClick={handleDeleteAccount}>
                 Eliminar su Cuenta
               </Button>
             </Col>
@@ -100,5 +105,5 @@ export default function MyProfile() {
         </Card.Body>
       </Container>
     </div>
-  )
+  );
 }
