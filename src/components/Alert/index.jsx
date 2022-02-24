620a7849d475d4a281c34fc94a6bfc6148fa5f60
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-24 07:45:38 -0300

    comments fix: se hace const de variables repetidas

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,41 +1,46 @@
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
+
+const errorIcon = 'error';
+const infoIcon = 'info';
+const successIcon = 'success';
+const warningIcon = 'warning';
 
 export function InfoAlert(title, message) {
-  return Swal.fire(title, message, "info");
+  return Swal.fire(title, message, infoIcon);
 }
 
 export function ErrorAlert(title, message) {
-  return Swal.fire(title, message, "error");
+  return Swal.fire(title, message, errorIcon);
 }
 
 export function SuccessAlert(title, message) {
-  return Swal.fire(title, message, "success");
+  return Swal.fire(title, message, successIcon);
 }
 
 export function ConfirmAlert(title, message) {
   return Swal.fire({
     title: title,
     text: message,
-    icon: "warning",
+    icon: warningIcon,
     showDenyButton: true,
-    confirmButtonText: "Confirmar",
-    denyButtonText: `Cancelar`,
-    confirmButtonColor: "#78D052",
-    cancelButtonColor: "#d33",
+    confirmButtonText: 'Confirmar',
+    denyButtonText: 'Cancelar',
+    confirmButtonColor: '#78D052',
+    cancelButtonColor: '#d33',
   }).then((result) => {
     if (result.isConfirmed) {
       Swal.fire({
-        title: "Confirmado",
-        text: "",
-        icon: "success",
-        confirmButtonColor: "#78D052",
+        title: 'Confirmado',
+        text: '',
+        icon: successIcon,
+        confirmButtonColor: '#78D052',
       });
     } else {
       Swal.fire({
-        title: "Cancelado",
-        text: "",
-        icon: "info",
-        confirmButtonColor: "#78D052",
+        title: 'Cancelado',
+        text: '',
+        icon: infoIcon,
+        confirmButtonColor: '#78D052',
       });
     }
     return result;
