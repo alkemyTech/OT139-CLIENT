810231b48f09b3882f6d4921d266dbff0b5f993a
tamiisoledad <tamiisoledad4@gmail.com>
2022-02-24 00:32:15 -0300

    se elimino un comentario

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginForm from './components/LoginForm/LoginForm';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <App /> */}
-      <LoginForm/>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
