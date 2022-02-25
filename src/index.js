ce81b118dcf23d1878c7bd6defb26f91257a7454
tamiisoledad <tamiisoledad4@gmail.com>
2022-02-25 16:47:20 -0300

    Se elimino comentario

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-     {/*  <App /> */}
-     <Home/>
+     <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
