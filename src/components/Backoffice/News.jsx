0c56c75b7666d312a24a3db0c8f391d4887eef14
Hugo Venega <hugo.venega.00@gmail.com>
2022-03-02 16:27:14 -0300

    Añadir tabla de novedades

---
+++ src/components/Backoffice/News.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function News() {
+  return (
+    <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">imagen</th>
+      <th scope="col">Día de creación</th>
+      <th scope="col">editar</th>
+    </tr>
+  </thead>
+  <tbody>
+  {}
+    <tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+      );
+}
+
+export default News;
