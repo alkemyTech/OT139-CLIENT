9460361e66ba44ec2bbdc3c44f7de3d50719ce8f
AndresHense <andreshense@gmail.com>
2022-03-02 16:59:01 -0300

    wrap body into bootstrap container

--- src/screens/backOffice/UsersList.jsx
+++ src/screens/backOffice/UsersList.jsx
@@ -1,5 +1,5 @@
 import BackOffice from '../../layout/backOffice/BackOffice';
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, Container } from 'react-bootstrap';
 
 import React from 'react';
 
@@ -16,37 +16,39 @@ const UsersList = () => {
 
   return (
     <BackOffice>
-      <Table striped bordered hover responsive className='table-sm'>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Email</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, id) => (
-            <tr key={id}>
-              <td>{user.name}</td>
-              <td>{user.surname}</td>
-              <td>{user.email}</td>
-              <td>
-                <Button variant='light' className='btn-sm'>
-                  <i className='fas fa-edit'></i>
-                </Button>
-                <Button
-                  variant='danger'
-                  className='btn-sm'
-                  onClick={() => deleteHandler(user._id)}
-                >
-                  <i className='fas fa-trash'></i>
-                </Button>
-              </td>
+      <Container>
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Email</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {users.map((user, id) => (
+              <tr key={id}>
+                <td>{user.name}</td>
+                <td>{user.surname}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Button variant='light' className='btn-sm'>
+                    <i className='fas fa-edit'></i>
+                  </Button>
+                  <Button
+                    variant='danger'
+                    className='btn-sm'
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <i className='fas fa-trash'></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </BackOffice>
   );
 };
