08f47c6a899c423076238163c6f48ad24ac93aae
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-02 17:18:49 -0300

    Manejo de errores en el componente MyProfile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -4,10 +4,13 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import ErrorCard from '../ErrorCard/ErrorCard';
 import './myProfile.css';
 
 export default function MyProfile() {
-  const [profileData, setProfileData] = useState();
+  const [profileData, setProfileData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleEditProfile = () => {
     // @TODO Implementar la funcionalidad del botón Editar Cuenta
@@ -19,20 +22,33 @@ export default function MyProfile() {
 
   useEffect(() => {
     async function getProfileData() {
-      // @TODO Implementar una vez que la API esté lista
-      // Llamar a un endpoint de Autenticación con los datos del usuario
-      setProfileData({
-        firstName: 'John',
-        lastName: 'Smith',
-        email: 'johnsmith@example.com',
-      });
+      try {
+        setIsLoading(true);
+        // @TODO Implementar una vez que la API esté lista
+        // Llamar a un endpoint de Autenticación con los datos del usuario
+        const response = await Promise.resolve({ ok: true });
+        if (!response.ok) {
+          throw new Error('Data could not be retrieved');
+        }
+
+        const data = {
+          firstName: 'John',
+          lastName: 'Smith',
+          email: 'johnsmith@example.com',
+        };
+
+        setProfileData(data);
+      } catch (err) {
+        setError(true);
+      }
+      setIsLoading(false);
     }
 
     getProfileData();
   }, []);
 
   return (
-    <div className='max_width_wrapper_medium mx-auto'>
+    <div className='max_width_wrapper_medium mx-auto my-4'>
       <Container as={Card} fluid='md'>
         <Card.Body className='px-md-5'>
           <Row className='mb-3'>
@@ -40,21 +56,19 @@ export default function MyProfile() {
               <div className='title_large'>Mi Perfil</div>
             </Col>
           </Row>
-          {!profileData ? (
-            'Cargando...'
-          ) : (
+          <Row className='mb-4'>
+            <Col>
+              <div className='subtitle'>Datos de Cuenta</div>
+            </Col>
+          </Row>
+          {!isLoading && profileData && (
             <>
-              <Row className='mb-4'>
-                <Col>
-                  <div className='subtitle'>Datos de Cuenta</div>
-                </Col>
-              </Row>
               <Row className='mb-4'>
                 <Col className='disableText' sm='6'>
                   Nombre
                 </Col>
                 <Col className='text_strong' sm='6'>
-                  {profileData.firstName}
+                  {profileData?.firstName}
                 </Col>
               </Row>
               <Row className='mb-4'>
@@ -62,7 +76,7 @@ export default function MyProfile() {
                   Apellido
                 </Col>
                 <Col className='text_strong' sm='6'>
-                  {profileData.lastName}
+                  {profileData?.lastName}
                 </Col>
               </Row>
               <Row className='mb-4'>
@@ -70,18 +84,29 @@ export default function MyProfile() {
                   E-Mail
                 </Col>
                 <Col className='text_strong' sm='6'>
-                  {profileData.email}
+                  {profileData?.email}
+                </Col>
+              </Row>
+              <Row className='mb-5'>
+                <Col>
+                  <Button
+                    disabled={!profileData}
+                    size='lg'
+                    onClick={handleEditProfile}
+                  >
+                    Editar Cuenta
+                  </Button>
                 </Col>
               </Row>
             </>
           )}
-          <Row className='mb-5'>
-            <Col>
-              <Button disabled={!profileData} size='lg' onClick={handleEditProfile}>
-                Editar Cuenta
-              </Button>
-            </Col>
-          </Row>
+          {!isLoading && error && (
+            <ErrorCard
+              title='Algo salio mal procesando su solicitud.'
+              text='Por favor vuelva a intentarlo en unos instantes.'
+            />
+          )}
+          {isLoading && <p>Cargando...</p>}
           <Row className='mb-3'>
             <Col>
               <div className='subtitle color_danger'>Eliminar Cuenta</div>
