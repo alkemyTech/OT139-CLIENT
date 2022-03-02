19b9a35e057a95e2180612eb295e127264fe1f43
Hugo Venega <hugo.venega.00@gmail.com>
2022-03-02 16:36:17 -0300

    Añadir funcionalidad para obtener datos desde un endpoint

--- src/components/Backoffice/News.jsx
+++ src/components/Backoffice/News.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function News() {
+
+  const [entries, setEntries] = useState([]);
+  const [errors, setError] = useState([]);
+
+  useEffect(() => {
+    async function getBackOfficeNews() {
+
+      try {
+        const response = await fetch("");
+        const data = await response.json();
+        setEntries(data.entries);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    getBackOfficeNews();
+  });
+
   return (
     <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">imagen</th>
-      <th scope="col">Día de creación</th>
-      <th scope="col">editar</th>
-    </tr>
-  </thead>
-  <tbody>
-  {}
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-  </tbody>
-</table>
-      );
+      <thead>
+        <tr>
+          <th scope="col">Nombre</th>
+          <th scope="col">imagen</th>
+          <th scope="col">Día de creación</th>
+          <th scope="col">editar</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+        entries.map((entriesItem) => {
+          return (
+            <tr>
+            <th scope="row">{entriesItem.name}</th>
+            <td>{entriesItem.imageUrl}</td>
+            <td>{entriesItem.createdAt}</td>
+            <td>editar/borrar</td> {//TODO @implementar la funcionalidad de editar y borrar }
+          </tr>
+          )
+        })
+        }
+      </tbody>
+    </table>
+  );
 }
 
 export default News;
