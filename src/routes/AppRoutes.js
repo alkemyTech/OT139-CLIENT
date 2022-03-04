25b885c1d73b8e3600e873eeadfe602524ef1758
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-03-03 21:55:57 -0300

    create component ActivityView and 404 page for activity

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from '../components/Home/Home.jsx';
 import About from '../components/About/About.jsx';
+import ActivityView from '../components/Activities/ActivityView';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/actividades/:id" element={<ActivityView />}/>
     </Routes>
   )
 }
