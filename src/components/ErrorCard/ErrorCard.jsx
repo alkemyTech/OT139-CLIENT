54873b10bbf3065ef2e5e5c9bb5d49dfed015e36
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-07 10:19:01 -0300

    Ajustando estilos del componente ErrorCard

--- src/components/ErrorCard/ErrorCard.jsx
+++ src/components/ErrorCard/ErrorCard.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import './errorCard.css';
 
 export default function ErrorCard({ title, text }) {
   return (
-    <Card border='danger' className='my-4'>
+    <Container as={Card} fluid='md' border='danger'>
       <Card.Body>
         <img
-          className='error_card_svg mb-4'
+          className='error_card_svg mx-auto mb-4'
           src='images/assets/page-not-found.svg'
           alt='error resource not found'
         />
         <h4 className='color_danger text-center'>{title}</h4>
         <p className='color_danger text-center'>{text}</p>
       </Card.Body>
-    </Card>
+    </Container>
   );
 }
