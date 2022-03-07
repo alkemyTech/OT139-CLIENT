45501b2250109b2d9d73cfea0de8bdb119c2f998
ema <lotofacundo6@gmail.com>
2022-03-07 15:57:17 -0300

    OT139-27-screen-contacto-fix reestableciendo componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
+import AppRoutes from './routes/AppRoutes'
 function App() {
   return (
     <BrowserRouter>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
