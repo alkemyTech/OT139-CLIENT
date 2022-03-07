8b2e85d52c1f948caff448311e5c7a46392bc5d4
ema <lotofacundo6@gmail.com>
2022-03-07 11:07:11 -0300

    OT139-27-formulario-contacto-fix err codigo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
     <BrowserRouter>
+    <AppRoutes/>
     </BrowserRouter>
   );
 }
