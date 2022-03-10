520d76e09234b1645d309c8f91299167509e7395
tamiisoledad <tamiisoledad4@gmail.com>
2022-03-10 14:58:08 -0300

    useEffect corregido con mockup

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Slider from '../Slider/Slider';
 import style from './home.module.css';
 import './home.css';
-import { useFetch } from '../../hooks/useFetch';
 
 const Home = () => {
-  const welcomeText = useFetch(url);
-  const dataSlide = useFetch(url);
+  const [data, setData] = useState({welcome: "", slide: []});
+  const welcomeText = "Bienvenidos";
+  const dataSlide = [
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1605457867610-e990b192418e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1126&q=80',
+      text: 'Slide uno',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1561424412-6c2125ecb1cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      text: 'Slide 2',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1513972400628-4ae6371f9124?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      text: '',
+    },
+  ];
+
+useEffect(() => {
+    setData({
+      ...data,
+      welcome: welcomeText,
+      slide: dataSlide})
+  }, [welcomeText, dataSlide]);
+
+  
 
     return(
       <div id='home'>
-            <Slider data={dataSlide}/> 
-            <h1 className={style.title}>{welcomeText}</h1> 
-
+            <Slider data={data.slide}/> 
+            <h1 className={style.title}>{data.welcome}</h1> 
             <h2 className={style.subtitle}>Ultimas novedades</h2>
             <div className={style.news_container}>
               <article className={style.news}></article>
