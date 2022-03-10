38dc1b694b339933f5d7f7ea187fddc623c533fe
Yonpalac <yon.palac@gmail.com>
2022-03-10 12:33:52 +0100

    Agregue ruta y cambie nombre de carpeta de screens a views

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from '../components/Home/Home.jsx';
 import About from '../components/About/About.jsx';
+import { News } from '../views/public/News.jsx';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/news" element={<News />} />
     </Routes>
   )
 }
