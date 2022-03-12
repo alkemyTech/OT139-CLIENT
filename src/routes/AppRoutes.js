5095bc5fbf7d8d3f58b3fe22a158d8e6bcbd1d05
ema <lotofacundo6@gmail.com>
2022-03-12 11:58:40 -0300

    Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Home/Home.jsx';
 import About from '../components/About/About.jsx';
-import ContactForm from '../views/Contact/Contact.jsx';
+import Contact from '../views/Contact/Contact.jsx';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<ContactForm/>}/>
+      <Route path="/contact" element={<Contact/>}/>
     </Routes>
   )
 };
