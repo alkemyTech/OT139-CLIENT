d7f5a17a7ce6df819fec5a61e1c8909c5c01e3b1
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2022-03-12 12:49:43 -0300

    OT139 27 screen contacto creado (#16)
    * OT139-27-Screen-Contacto-make design responsive
    
    * OT139-27-Screen-Contacto create
    
    * OT139-27-screen-contact fix-code of FormContact
    
    * OT139-27-screen-contacto-fix problemas con package-lock
    
    * OT139-27-screen-loader-fix package-lock
    
    * OT139-27-Screen-Contacto-fix err package-lock
    
    * OT139-27-screen-loader-rev package-lock
    
    * OT-139-screen-contact-delete package-lock
    
    * Delete package-lock.json
    
    * OT139-screen-contact-add package-lock
    
    * Update package-lock.json
    
    * OT139-27-formulario-contacto-fix err codigo
    
    * OT139-27-screen-contacto-fix err package-lock
    
    * OT139-27-screen-contacto-fix reestableciendo componentes
    
    * OT139-27-screen-contacto-cambios en package-lock
    
    * OT139-27-screen-contacto-cambios en codigo
    
    * OT139-27-Screen-Contacto-cambios de archivos
    
    * OT139-27-Screen-contacto-update code contact
    
    * Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Home/Home.jsx';
 import About from '../components/About/About.jsx';
+import Contact from '../views/Contact/Contact.jsx';
 
 
 export default function AppRoutes() {
@@ -9,6 +10,7 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact/>}/>
     </Routes>
   )
-}
+};
