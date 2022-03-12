34ec1fee2dd9d89288f4f5db52333f8db7848bc5
Fran <palacios.francisco@outlook.com>
2022-03-12 17:04:19 -0300

    Eliminacion de componente Slide de Home y la constante dataSLider

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,29 +7,6 @@ import './home.css';
 const Home = () => {
   const [data, setData] = useState({ welcome: '', slide: [], news: []});
   const welcomeText = 'Bienvenidos';
-  const dataSlide = [
-    {
-      imageUrl:
-        'https://images.unsplash.com/photo-1605457867610-e990b192418e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1126&q=80',
-      text: 'Slide uno',
-    },
-    {
-      imageUrl:
-        'https://images.unsplash.com/photo-1561424412-6c2125ecb1cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      text: 'Slide 2',
-    },
-    {
-      imageUrl:
-        'https://images.unsplash.com/photo-1513972400628-4ae6371f9124?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      text: '',
-    },
-  ];
-  const news = [
-    {
-      imageUrl: '',
-      text: ''
-    }
-  ]
 
   useEffect(() => {
     setData({
@@ -42,7 +19,6 @@ const Home = () => {
 
   return (
     <div id='home'>
-      <Slider data={data.slide} />
       <h1 className={style.title}>{data.welcome}</h1>
       <h2 className={style.subtitle}>Ultimas novedades</h2>
       <div className={style.news_container}>
