9d3faae9834a9df9d0d853889e4f8ebfccec9634
Yonpalac <yon.palac@gmail.com>
2022-03-12 20:58:45 +0100

    cambio estilos a tres columnas

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Nav } from "react-bootstrap";
-import {
-  Twitter,
-  Facebook,
-  Vimeo,
-  Linkedin,
-  Youtube
-} from "react-bootstrap-icons";
+import { Container, Row, Col } from "react-bootstrap";
 import './footer.css';
 
 export const Footer = () => {
-  const [logo, setLogo] = useState("");
+  const [logo, setLogo] = useState([]);
   const [errors, setErrors] = useState([]);
-  const today = new Date();
-  const year = today.getFullYear();
 
   useEffect(() => {
     async function getFooterData() {
@@ -26,57 +17,34 @@ export const Footer = () => {
         setErrors(error);
       }
     }
+    
     getFooterData();
   }, []);
 
   return (
     <footer className='footer'>
-        <div className='footer-nav'>
-            <div className='footer-nav_column'>
-                <Nav.Item>
-                  <Link to={`/activities`}>Actividades</Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to={`/news`}>Novedades</Link>
-                </Nav.Item>
-            </div>
-            <div className='footer-nav_column'>
-              {!logo ? "Logo somos mas" : <img src={logo} alt="Logo somos mas"></img>
+      <Container>
+        <Row>
+          <Col className='footer-links'>
+            <Link to={`/activities`}>Actividades</Link>
+            <Link to={`/news`}>Novedades</Link>
+            <Link to={`/testimonials`}>Testimonios</Link>
+            <Link to={`/testimonials`}>Testimonios</Link>
+            <Link to={`/testimonials`}>Testimonios</Link>
+          </Col>
+          <Col>
+            {!logo ? "Logo somos mas" : <img src={logo} alt="Logo somos mas"></img>
               }
-            </div>
-            <div className='footer-nav_column'>
-                <Nav.Item>
-                  <Link to={`/testimonials`}>Testimonios</Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to={`/about`}>Nosotros</Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to={`/contacts`}>Contacto</Link>
-                </Nav.Item>
-            </div>
-        </div>
-        <div className='footer-social'>
-            <div className='footer-social_icons'>
-              <a href={``}>
-                <Facebook className="icon"></Facebook>
-              </a>
-              <a href={``}>
-                <Twitter className="icon"></Twitter>
-              </a>
-              <a href={``}>
-                <Vimeo className="icon"></Vimeo>
-              </a>
-              <a href={``}>
-                <Linkedin className="icon"></Linkedin>
-              </a>
-              <a href={``}>
-                <Youtube className="icon"></Youtube>
-              </a>
-
-              <p>{ year } by Alkemy. All Rights Reserved</p>
-            </div>
-        </div>
+          </Col>
+          <Col className="footer-links">
+            <a href={``}>Facebook</a>
+            <a href={``}>Twitter</a>
+            <a href={``}>Vimeo</a>
+            <a href={``}>Linkedin</a>
+            <a href={``}>Youtube</a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
