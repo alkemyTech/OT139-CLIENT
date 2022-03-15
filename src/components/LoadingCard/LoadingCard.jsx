529b395fbc96eeae29c010831ceff6169db9685c
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-15 05:31:54 -0300

    Ajustando estilos de ErrorCard y LoadingCard

--- src/components/LoadingCard/LoadingCard.jsx
+++ src/components/LoadingCard/LoadingCard.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import Spinner from 'react-bootstrap/Spinner';
+import Loader from '../loader/Loader.jsx';
 
 export default function LoadingCard() {
   return (
     <Container as={Card} fluid='md'>
-      <Card.Body className='text-center mx-auto my-4'>
-        <Spinner animation='border' role='status'>
-          <span className='visually-hidden'>Cargando...</span>
-        </Spinner>
-        <p>Cargando</p>
+      <Card.Body className='text-center mx-auto my-4 py-5'>
+        <Loader />
       </Card.Body>
     </Container>
   );
