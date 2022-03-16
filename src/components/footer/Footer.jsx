b088c9cc29c466254b04b2c8acb1327bcb64c87d
Hugo Venega <hugo.venega.00@gmail.com>
2022-03-16 18:15:20 -0300

    Implementar muestra dinamica

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,22 +3,20 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col } from "react-bootstrap";
 import './footer.css';
 
+const footerData = {
+  logo: "https://images.squarespace-cdn.com/content/v1/5f62b687cae73d2408a06539/1602807735303-4W086W30YX6B3D23N04L/image-asset.png"
+}
+const footerSocialMedias = [{ url: "facebook.com", name: "Facebook" }, { url: "twitter.com", name: "Twitter" }, { url: "vimeo.com", name: "Vimeo" }, { url: "linkedin.com", name: "LinkedIn" }, { url: "youtube.com", name: "Youtube" }]
+
 export const Footer = () => {
   const [logo, setLogo] = useState([]);
+  const [socialMedias, setSocialMedias] = useState([footerSocialMedias]);
   const [errors, setErrors] = useState([]);
 
   useEffect(() => {
-    async function getFooterData() {
-      try {
-        const response = await fetch("http://localhost:3000/organizations/1/public");
-        const data = await response.json();
-        setLogo(data.image);
-      } catch(error) {
-        setErrors(error);
-      }
-    }
-    
-    getFooterData();
+    //TODO @Implementar obtencion de datos desde endpoint
+    setLogo(footerData)
+    setSocialMedias(footerSocialMedias);
   }, []);
 
   return (
@@ -27,18 +25,17 @@ export const Footer = () => {
         <Row>
           <Col>
             {!logo ? "Logo somos mas" : <img src={logo} alt="Logo somos mas"></img>
-              }
+            }
           </Col>
           <Col className='footer-links'>
             <Link to={`/about`}>Acerca</Link>
             <Link to={`/contact`}>Nosotros</Link>
           </Col>
           <Col className="footer-links">
-            <a href={``}>Facebook</a>
-            <a href={``}>Twitter</a>
-            <a href={``}>Vimeo</a>
-            <a href={``}>Linkedin</a>
-            <a href={``}>Youtube</a>
+            {socialMedias.map(socialMediaItem => {
+              return (
+                <a href={`${socialMediaItem.url}`}>{socialMediaItem.name}</a>)
+            })}
           </Col>
         </Row>
       </Container>
