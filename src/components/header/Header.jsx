cf8b3723b9a83f7519e975e9e556a92e64330cff
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-17 00:39:34 -0300

    Ahora el Header carga el logo del Endpoint /organizations/1/public

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,48 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import CustomLink from '../CustomLink/CustomLink.jsx';
+import { get } from '../../services/apiService';
 import './header.css';
 
-const headerData = {
-  logo: 'https://images.squarespace-cdn.com/content/v1/5f62b687cae73d2408a06539/1602807735303-4W086W30YX6B3D23N04L/image-asset.png',
-  menu: [
-    {
-      name: 'Inicio',
-      url: '/',
-    },
-    {
-      name: 'Nosotros',
-      url: '/about',
-    },
-    {
-      name: 'Contacto',
-      url: '/contact',
-    },
-  ],
-};
+const menu = [
+  {
+    name: 'Inicio',
+    url: '/',
+  },
+  {
+    name: 'Nosotros',
+    url: '/about',
+  },
+  {
+    name: 'Contacto',
+    url: '/contact',
+  },
+];
 
-export function Header() {
+export default function Header() {
   const [logo, setLogo] = useState('');
-  const [menu, setMenu] = useState([]);
-  const [errors, setError] = useState([]);
+  const [errors, setError] = useState(null);
 
   useEffect(() => {
-    //TODO @Implementar obtencion de datos desde endpoint
-    setLogo(headerData.logo);
-    setMenu(headerData.menu);
+    async function getOrganizationLogo() {
+      const response = await get('/organizations/1/public');
+      const isValidResponse = response && !(response instanceof Error);
+
+      if (isValidResponse) {
+        setLogo(response.logo);
+      } else {
+        setError(true);
+      }
+    }
+
+    getOrganizationLogo();
   }, []);
 
   return (
     <ul>
-      <img
-        src={logo}
-        width='auto'
-        height='40'
-        className='d-inline-block align-top'
-        alt='Logo somos más'
-      />
+      {!errors && (
+        <img
+          src={logo}
+          width='auto'
+          height='40'
+          className='d-inline-block align-top'
+          alt='Logo somos más'
+        />
+      )}
       {menu.map((menuItem) => {
         return (
-          <li>
+          <li key={menuItem.name}>
             <CustomLink to={menuItem.url}>
               {menuItem.name}
             </CustomLink>
