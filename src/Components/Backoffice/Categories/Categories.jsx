4e4e2249c35cb42ea55084844ed7181426cb6a95
Yonpalac <yon.palac@gmail.com>
2022-03-17 21:19:46 +0100

    Agregue backoffice/categories para listar

---
+++ src/Components/Backoffice/Categories/Categories.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Container } from 'react-bootstrap';
+
+export const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    async function getCategoriesData() {
+      try {
+        const response = await fetch('http://localhost:3000/categories');
+        const data = await response.json();
+        setCategories(data.image);
+      } catch (error) {
+        setErrors(error);
+      }
+    }
+
+    getCategoriesData();
+  }, []);
+
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories?.length ? (
+            categories.map((category) => {
+              return (
+                <tr>
+                  <td>{category.name}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr className='text-center'>No se encontraron Actividades</tr>
+          )}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
