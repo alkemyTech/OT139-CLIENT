8bbe976de60a0ad802129c4e47995203dff8876e
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-17 22:14:14 -0300

    Se realizaron ajustes en el response de apiService

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { getPrivateHeaders } from './authorizationHeaders';
-axios.defaults.baseURL = process.env.BASE_URL;
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 export const post = async (endPoint, body) => {
-  const  headers = getPrivateHeaders();
+  const headers = getPrivateHeaders();
   const response = {};
-  
+
   try {
-    const data = await axios.post(endPoint, body, headers);
-    response.data = data;
+    const axiosResponse = await axios.post(endPoint, body, headers);
+    response.data = axiosResponse.data;
   } catch (error) {
     response.error = error;
   }
@@ -20,8 +20,8 @@ export const get = async (endPoint) => {
   const response = {};
 
   try {
-    const data = await axios.get(endPoint);
-    response.data = data;
+    const axiosResponse = await axios.get(endPoint);
+    response.data = axiosResponse.data;
   } catch (error) {
     response.error = error;
   }
