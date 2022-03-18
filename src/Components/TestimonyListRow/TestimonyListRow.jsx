903e07d4a82c3f6ef1079ab3256e97f4ed99627c
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-18 17:33:25 -0300

    Se creo el esqueleto para la screen listado de testimonios

---
+++ src/Components/TestimonyListRow/TestimonyListRow.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TestimonyListRow() {
+  return <div>TestimonyListRow</div>;
+}
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,16 +6,20 @@ import { News } from '../views/News/News.jsx';
 import UsersList from '../views/backOffice/UsersList.jsx';
 import NewDetail from '../views/NewDetail/NewDetail.jsx';
 import Contact from '../views/Contact/Contact.jsx';
+import Testimonials from '../views/Testimonials/Testimonials.jsx';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
-      <Route path='/backoffice/users' element={<UsersList />} />
       <Route path="/news" element={<News />} />
       <Route path='/news/:id' element={<NewDetail />} />
       <Route path='/contact' element={<Contact />} />
+      <Route path="/backoffice">
+        <Route path='users' element={<UsersList />} />
+        <Route path='testimonials' element={<Testimonials />} />
+      </Route>
     </Routes>
   );
 }
