b8a510c18c98b6b6cf90020c5fc0f477e1d3f00e
Yonatan <76592537+YonPalac1@users.noreply.github.com>
2022-03-19 12:05:21 -0300

    OT139 47 screen de novedades publica (#32)
    * Creado screen de novedades publica
    
    * Cambie de path la carpeta screens
    
    * Agregue ruta y cambie nombre de carpeta de screens a views
    
    * Agrego endpoint a listado de novedades
    
    * Arreglo conflictos
    
    * Agrego apiService y errorAlert

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Home/Home.jsx';
 import About from '../components/About/About.jsx';
+import { News } from '../views/News/News.jsx';
 import UsersList from '../views/backOffice/UsersList.jsx';
 import NewDetail from '../views/NewDetail/NewDetail.jsx';
 import Contact from '../views/Contact/Contact.jsx';
@@ -12,6 +13,7 @@ export default function AppRoutes() {
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
       <Route path='/backoffice/users' element={<UsersList />} />
+      <Route path="/news" element={<News />} />
       <Route path='/news/:id' element={<NewDetail />} />
       <Route path='/contact' element={<Contact />} />
     </Routes>
---
+++ src/views/News/News.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import { get } from '../../services/apiService';
+import { ErrorAlert } from '../../components/Alert';
+
+export const News = () => {
+  const [news, setNews] = useState([]);
+  const url = `http://localhost:3000/news`;
+
+  useEffect(async () => {
+    const { data, error } = await get(url);
+    if (error) {
+      ErrorAlert('Error!', 'Ocurrio un error');
+    } else {
+      setNews(data);
+    }
+  }, [url]);
+
+  return (
+    <Container>
+      <Row>
+        {news.map((item) => {
+          return (
+            <Col className='mt-5'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant='top' src={item.image} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Link to={`/news/${item.id}`} variant='primary'>
+                    Ver noticia
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
