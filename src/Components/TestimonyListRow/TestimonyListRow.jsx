51fcc4d8bdac7415e30bd7778f62e900fb17826e
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-20 20:47:05 -0300

    Se creo el esqueleto para las operaciones de ABM de testimonios

--- src/Components/TestimonyListRow/TestimonyListRow.jsx
+++ src/Components/TestimonyListRow/TestimonyListRow.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import './testimonyListRow.css';
 
-export default function TestimonyListRow({ name }) {
+export default function TestimonyListRow({
+  name,
+  onEdit: handleEdit,
+  onDelete: handleDelete,
+}) {
   return (
     <div className='testimony_list_row'>
       <div className='testimony_list_row__name'>{name}</div>
       <div className='testimony_list_row__action_buttons'>
-        <button type='button' className='testimony_list_row__button'>
+        <button
+          type='button'
+          onClick={handleEdit}
+          className='testimony_list_row__button'
+        >
           <i className='fa-solid fa-pencil'></i>
-        </button> 
-        <button type='button' className='testimony_list_row__button'>
+        </button>
+        <button
+          type='button'
+          onClick={handleDelete}
+          className='testimony_list_row__button'
+        >
           <i className='fa-solid fa-trash'></i>
         </button>
       </div>
--- src/views/Testimonials/Testimonials.jsx
+++ src/views/Testimonials/Testimonials.jsx
@@ -6,7 +6,19 @@ import './testimonials.css';
 
 export default function Testimonials() {
   const [testimonials, setTestimonials] = useState([]);
-  
+
+  const handleAddTestimony = () => {
+    // @TODO: Integrar con el formulario de edición de testimonios
+  };
+
+  const handleEditTestimony = (id) => () => {
+    // @TODO: Integrar con el formulario de edición de testimonios
+  };
+
+  const handleDeleteTestimony = (id) => () => {
+    // @TODO: Integrar con el endpoint DELETE /testimonials
+  };
+
   useEffect(() => {
     async function getTestimonials() {
       const { data, error } = await get('/testimonials');
@@ -28,15 +40,27 @@ export default function Testimonials() {
             Estos son los testimonios disponibles de nuestros socios.
           </h3>
         </div>
-        <button type='button' className='testimonials__button'>
+        <button
+          type='button'
+          onClick={handleAddTestimony}
+          className='testimonials__button'
+        >
           + Nuevo
         </button>
       </header>
-      <div class='testimony_list'>
-        {(testimonials?.length === 0 || !testimonials) && <TestimonyListEmptyRow />}
-        {testimonials && testimonials.map(testimony => (
-          <TestimonyListRow key={testimony.id} name={testimony.name} />
-        ))}
+      <div className='testimony_list'>
+        {(testimonials?.length === 0 || !testimonials) && (
+          <TestimonyListEmptyRow />
+        )}
+        {testimonials &&
+          testimonials.map((testimony) => (
+            <TestimonyListRow
+              key={testimony.id}
+              name={testimony.name}
+              onEdit={handleEditTestimony(testimony.id)}
+              onDelete={handleDeleteTestimony(testimony.id)}
+            />
+          ))}
       </div>
     </div>
   );
