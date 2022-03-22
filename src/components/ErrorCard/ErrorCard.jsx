f0ac547df050fa730325443c96130cbdac45465c
Eric Varela Avalos <45970652+evarelaavalos@users.noreply.github.com>
2022-03-22 10:36:32 -0300

    OT139-50 Screen Edición Organización (admin) (Frontend) (#40)
    * Creada la screen EditOrganization
    
    * Incorporada validacion al formulario con Formik
    
    * Separacion de validadores en funciones individuales
    
    * Creada funcion para obtener los datos de la organizacion del backend
    
    * Se arreglo error con la ruta de la imagen de ErrorCard
    
    * Se agrego funcionalidad al botón cancelar
    
    * Se creo la logica para el manejo de errores y el estado cargando
    
    * Se incorporo el metodo PUT al servicio de peticiones de HTTP
    
    * Creada funcion para modificar datos de la organizacion
    
    * Ajustando estilos de ErrorCard y LoadingCard
    
    * PR Fixes: Reutilizando colores de los estilos compartidos
    
    * PR Fixes: Cambio en la nomenclatura de los archivos
    
    * PR Fixes: Creada una funcion para manejar los estilos
    
    * Creado valores por defecto para ErrorCard
    
    * Revirtiendo cambio a .env
    
    * PR Fixes: Se realizaron ajustes en la nomenclatura de los estilos
    
    * PR Fixes: Se adapto el codigo a la nueva implementacion del servicio de API
    
    * Se realizaron ajustes al metodo PUT del servicio de API

--- src/components/ErrorCard/ErrorCard.jsx
+++ src/components/ErrorCard/ErrorCard.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import '../../shared/style/colorOng.css';
 import './errorCard.css';
 
 export default function ErrorCard({ title, text }) {
+  const defaults = {
+    title: 'Algo salio mal procesando su solicitud.',
+    text: 'Por favor vuelva a intentarlo en unos instantes.',
+  }
+
   return (
     <Container as={Card} fluid='md' border='danger'>
       <Card.Body>
         <img
           className='error_card_svg mx-auto mb-4'
-          src='images/assets/page-not-found.svg'
+          src={`${process.env.PUBLIC_URL}/images/assets/page-not-found.svg`}
           alt='error resource not found'
         />
-        <h4 className='color_danger text-center'>{title}</h4>
-        <p className='color_danger text-center'>{text}</p>
+        <h4 className='color_red_ong text-center'>
+          {title ?? defaults.title}
+        </h4>
+        <p className='color_red_ong text-center'>
+          {text ?? defaults.text}
+        </p>
       </Card.Body>
     </Container>
   );
---
+++ src/views/OrganizationEdit/OrganizationEdit.jsx
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import { get, put } from '../../services/apiService';
+import { SuccessAlert, ErrorAlert } from '../../components/Alert';
+
+import ErrorCard from '../../components/ErrorCard/ErrorCard';
+import LoadingCard from '../../components/LoadingCard/LoadingCard';
+
+import './organizationEdit.css';
+
+export default function OrganizationEdit() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const initialValues = { name: '', logo: '' };
+
+  const validateName = (values, errors) => {
+    if (!values.name) {
+      errors.name = 'Este campo es obligatorio';
+    } else if (values.name.length < 3) {
+      errors.name = 'Este campo debe tener 3 caracteres o más';
+    }
+  };
+
+  const validateLogo = (values, errors) => {
+    if (!values.logo) {
+      errors.logo = 'Este campo es obligatorio';
+    } else if (values.logo.length < 3) {
+      errors.logo = 'Este campo debe tener 3 caracteres o más';
+    }
+  };
+
+  const validate = (values) => {
+    const errors = {};
+
+    validateName(values, errors);
+    validateLogo(values, errors);
+
+    return errors;
+  };
+
+  const onSubmit = async (values) => {
+    const correctValues = {
+      name: values.name,
+      image: values.logo,
+    };
+
+    const { data, error } = await put('/organizations/1', correctValues);
+
+    if (!error && data?.ok) {
+      SuccessAlert(
+        'Felicidades',
+        'Se guardaron los cambios satisfactoriamente.'
+      );
+    } else {
+      ErrorAlert(
+        'Oops...',
+        'Hubo un error al guardar los cambios.'
+      );
+    }
+  };
+
+  const onCancel = () => {
+    navigate(-1);
+  };
+
+  const formik = useFormik({ initialValues, validate, onSubmit });
+
+  useEffect(() => {
+    async function getOrganizationData() {
+      setIsLoading(true);
+
+      const { data, error } = await get('/organizations/1/public');
+
+      if (!error) {
+        const formValues = {
+          name: data.name,
+          logo: data.image,
+        };
+
+        formik.setValues(formValues);
+      } else {
+        setError(true);
+      }
+
+      setIsLoading(false);
+    }
+
+    getOrganizationData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getInputStyles = (inputName) => {
+    return formik.touched[inputName] && formik.errors[inputName]
+      ? 'edit_form__input edit_form__invalid_input'
+      : 'edit_form__input';
+  };
+
+  return (
+    <div className='max_width_wrapper_small mx-auto mt-4'>
+      {isLoading && <LoadingCard />}
+      {!isLoading && error && <ErrorCard />}
+      {!isLoading && !error && (
+        <div className='edit_form'>
+          <h3 className='edit_form__heading'>
+            Administra los datos de la organización
+          </h3>
+          <form onSubmit={formik.handleSubmit}>
+            <div className='edit_form__input_group'>
+              <label
+                className='edit_form__label required_field'
+                htmlFor='edit-form_name'
+              >
+                Nombre
+              </label>
+              <input
+                className={getInputStyles('name')}
+                type='text'
+                id='edit-form_name'
+                name='name'
+                placeholder='Somos Más'
+                {...formik.getFieldProps('name')}
+              />
+              {formik.touched.name && formik.errors.name && (
+                <p className='edit_form__error_msg'>{formik.errors.name}</p>
+              )}
+            </div>
+            <div className='edit_form__input_group'>
+              <label
+                className='edit_form__label required_field'
+                htmlFor='edit-form_logo'
+              >
+                Logo
+              </label>
+              <input
+                className={getInputStyles('logo')}
+                type='text'
+                id='edit-form_logo'
+                name='logo'
+                placeholder='URL del Logo'
+                {...formik.getFieldProps('logo')}
+              />
+              {formik.touched.logo && formik.errors.logo && (
+                <p className='edit_form__error_msg'>{formik.errors.logo}</p>
+              )}
+            </div>
+            <button
+              type='submit'
+              className='edit_form__btn edit_form__btn_primary'
+            >
+              Guardar Cambios
+            </button>
+            <button
+              type='button'
+              onClick={onCancel}
+              className='edit_form__btn edit_form__btn_secondary'
+            >
+              Cancelar
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
