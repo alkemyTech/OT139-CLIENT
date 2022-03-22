387e39b80afb404c22e94f7912d630cfc43719b8
AndresHense <andreshense@gmail.com>
2022-03-22 14:18:24 -0300

    resolve comments

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,24 +1,6 @@
-import axios from 'axios';
-
-export const post = async (body) => {
-  const response = {};
+import { post } from './apiService';
+export const postContact = async (body) => {
   body.phone = '11543254';
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
 
-  try {
-    const { data } = await axios.post(
-      'http://localhost:3000/contacts',
-      body,
-      config
-    );
-    response.data = data;
-  } catch (error) {
-    response.error = error;
-  }
-
-  return response;
+  return post('http://localhost:3000/contacts', body);
 };
