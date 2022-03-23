94e96a913eb487b9b49542a99e9f8232417bbaa9
ema <lotofacundo6@gmail.com>
2022-03-23 01:29:14 -0300

    Update NovedadesForm.js

--- src/views/Novedades/NovedadesForm.js
+++ src/views/Novedades/NovedadesForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
 import { CKEditor } from 'ckeditor4-react';
@@ -9,29 +9,31 @@ import 'animate.css';
 import './NovedadesForm.css';
 
 export default function NovedadesForm(news) {
-    const { saveForm, handleSubmit } = useForm();
+    const { register, handleSubmit } = useForm();
     const [data, setData] = useState([]);
+    const [value, setValue] = useState([]);
     let method = "";
-    let url="";
+    let url = "";
 
-    if (news) {
+    if (news === []) {
+        method = "POST";
+        url = 'http://localhost:8000/news/news';
+    }
+    else {
         console.log(news)
         url = `http://localhost:8000/news/${news.id}`;
         method = "PATCH";
-    }
-    else {
-        method = "POST";
-        url = 'http://localhost:8000/news/news';
     };
 
-    const onSubmit = async data => {
+    const onSubmit = async function (data) {
         try {
+            console.log(data)
             const requestOptions = {
                 method: method,
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
             };
-            const response = await fetch( url, requestOptions);
+            const response = await fetch(url, requestOptions);
             console.log(response.status);
         } catch (err) {
         };
@@ -40,24 +42,28 @@ export default function NovedadesForm(news) {
     return (
         <div className="main">
             <div className="Novedades_Form animate__animated animate__fadeInLeft ">
-                <img src={Logo}  className="Logo" />
-                <Form >
+                <img src={Logo} className="Logo" />
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <h2 className="text-center">Crear Novedades</h2>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" >
                         <Form.Label>Titulo</Form.Label>
-                        <Form.Control type="text" placeholder="example Marie Curie" required />
+                        <Form.Control type="text" placeholder="example Marie Curie" required {...register('title')} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label>Imagen</Form.Label>
-                        <Form.Control type="file" placeholder="name@example.com" required />
+                        <Form.Control type="file" placeholder="name@example.com" required {...register('imageUrl')} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
                         <Form.Label>Contenido</Form.Label>
-                        <CKEditor />
+                        <CKEditor
+                            onChange={evt => console.log(evt)}
+                        />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
                         <Form.Label>Categoria</Form.Label>
-                        <Form.Control type="select" rows={6} required />
+                        <Form.Select aria-label="Default select example" required {...register('categoryId')}>
+                            <option value="news">News</option>
+                        </Form.Select>
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Enviar
