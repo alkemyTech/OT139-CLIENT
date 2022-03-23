971e91cfac5de0277f88644c4190ece2605e3958
Hugo Venega <hugo.venega.00@gmail.com>
2022-03-23 19:02:14 -0300

    Correccion de PR

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,16 +3,10 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col } from "react-bootstrap";
 import './footer.css';
 
-const footerData = {
-  logo: "https://images.squarespace-cdn.com/content/v1/5f62b687cae73d2408a06539/1602807735303-4W086W30YX6B3D23N04L/image-asset.png"
-}
-const footerSocialMedias = [{ url: "facebook.com", name: "Facebook" }, { url: "twitter.com", name: "Twitter" }, { url: "vimeo.com", name: "Vimeo" }, { url: "linkedin.com", name: "LinkedIn" }, { url: "youtube.com", name: "Youtube" }]
-
 export const Footer = () => {
   const [logo, setLogo] = useState([]);
   const [socialMedias, setSocialMedias] = useState([]);
   const arraySocialMedias = Object.entries(socialMedias);
-  console.log({array: arraySocialMedias})
   const [errors, setErrors] = useState([]);
 
   useEffect(() => {
