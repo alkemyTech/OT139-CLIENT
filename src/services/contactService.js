4c718035168508559430395d5be26b1572c54c73
AndresHense <andreshense@gmail.com>
2022-03-24 14:37:20 -0300

    resolve bug

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -2,5 +2,5 @@ import { post } from './apiService';
 export const postContact = async (body) => {
   body.phone = '11543254';
 
-  return post('http://localhost:3000/contacts', body);
+  return await post('http://localhost:3000/contacts', body);
 };
