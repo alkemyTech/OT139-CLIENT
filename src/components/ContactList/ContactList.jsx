4ede59b8107cf74e03799f91f08261b1abb48a34
tamiisoledad <tamiisoledad4@gmail.com>
2022-03-24 22:49:08 -0300

    se reutilizo el apiService

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Table} from 'react-bootstrap';
-import { useFetch } from '../../hooks/useFetch';
+import { get } from '../../services/apiService';
 
 const ContactList = () => {
-    const contacts = useFetch("http://localhost:3000/contacts");
+  const [data, setData] = useState({});
+    const url = "http://localhost:3000/contacts";
+
+    useEffect(()=>{
+      const contact = get(url);
+      setData({
+        ...data,
+        contact
+      })
+    }, [data]);
+
 
   return (
     <Table striped bordered hover>
@@ -16,12 +26,14 @@ const ContactList = () => {
         </tr>
       </thead>
       <tbody>
-          {contacts.map(contact => {
+          {data.map(contact => {
+            return (
               <tr>
               <td>{contact.id}</td>
               <td>{contact.name}</td>
               <td>{contact.email}</td>
             </tr>
+            );
           })}
       </tbody>
     </Table>
