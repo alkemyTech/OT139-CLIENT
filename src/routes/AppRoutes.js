97d591bfa786ce0134bf119d02ce0da76ba6db00
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-03-26 20:14:16 -0300

    OT139-60 Screen Listado de Actividades (#20)
    * 1- Libreria SweetAlert2. 2- Funcion para crear alertas personalisadas
    
    * PR review: cambios sugeridos en Setup Alerts
    
    * PR OT139-20:  comments fix
    
    * PR OT139-20-Setup-Alertas  review: comment fix
    
    * Rename Index.jsx to index.jsx
    
    Cambio de nombre de archivo
    
    * add sweetalert dependency
    
    * PR review OT139-20: Cambios en alerta de confirmacion (comments fix)
    
    * Comments fix: cambios en logica de ConfirmAlert
    
    * comments fix: se hace const de variables repetidas
    
    * init OT139-60-Screen-Listado-de-Actividades
    
    * OT139-60-Screen-Listado-de-Actividades
    
    * change ReactIcon to FontAwesome
    
    * cambios de estilo de tabla y logica de props
    
    * comment fix: delete components/Alert
    
    * cambio de formato en public/index.html
    
    * cambio de directorio del screen de Activities
    
    * commit resolve: estilo: mobile first, mejora codigo
    
    * actividades pedidas a endpoint y mensaje de 'cargando' y 'no hay actividades' dinamico
    
    * Se agrega endpoint de actividades
    
    * .
    
    * delete /testimonyForm (corresponde a otra tarea)
    
    * add yarn.lock
    
    * add apiService (async/await)
    
    * comments fix: add await

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,6 +6,7 @@ import { News } from '../views/News/News.jsx';
 import UsersList from '../views/backOffice/UsersList.jsx';
 import NewDetail from '../views/NewDetail/NewDetail.jsx';
 import Contact from '../views/Contact/Contact.jsx';
+import Activities from '../views/Activities/Activities.jsx';
 import LoginForm from '../components/LoginForm/LoginForm.jsx';
 import RegisterForm from '../components/RegisterForm/RegisterForm.jsx';
 import Activity from '../views/Activities/Activity'
@@ -31,11 +32,12 @@ export default function AppRoutes() {
       <Route path="/activity/:id" element={<Activity />}/>
       <Route path='/edituser' element={<UserEdit />} />
       <Route path='/myprofile' element={<MyProfile />} />
-      <Route path="/backoffice">
+      <Route path='/backoffice'>
         <Route path='users' element={<UsersList />} />
-        <Route path="edit-organization" element={<OrganizationEdit />} />
-        <Route path="categories" element={<Categories />} />
-        <Route path="news" element={<BackofficeNews />} />
+        <Route path='edit-organization' element={<OrganizationEdit />} />
+        <Route path='categories' element={<Categories />} />
+        <Route path='news' element={<BackofficeNews />} />
+        <Route path='activities' element={<Activities />} />
       </Route>
     </Routes>
   );
