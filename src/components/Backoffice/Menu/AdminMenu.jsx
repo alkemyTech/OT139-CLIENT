495d1135cbd2fc09a06cadfdd4487e764dc4fcf3
Fran <palacios.francisco@outlook.com>
2022-03-29 21:06:10 -0300

    Accion de cerrar sesion

--- src/components/Backoffice/Menu/AdminMenu.jsx
+++ src/components/Backoffice/Menu/AdminMenu.jsx
@@ -4,23 +4,36 @@ import { NavDropdown } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../../actions/userActions';
 import './menu.css';
+import { ConfirmAlert } from '../../../components/Alert/index';
 
 export const AdminMenu = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const logoutUser = () => {
-    dispatch(logout())
-    navigate('/');
-  }
+    ConfirmAlert('¿Está seguro que desea cerrar sesión?', '').then((result) => {
+      if (result.isConfirmed) {
+        dispatch(logout());
+        navigate('/');
+      }
+    });
+  };
 
   return (
     <NavDropdown title='Backoffice' id='basic-nav-dropdown'>
-      <Link to='/backoffice/news' className='links'>Novedades</Link>
-      <Link to='/backoffice/categories' className='links'>Categorias</Link>
-      <Link to='/backoffice/testimonials' className='links'>Testimonios</Link>
+      <Link to='/backoffice/news' className='links'>
+        Novedades
+      </Link>
+      <Link to='/backoffice/categories' className='links'>
+        Categorias
+      </Link>
+      <Link to='/backoffice/testimonials' className='links'>
+        Testimonios
+      </Link>
       <NavDropdown.Item className='links'>
-        <button className="item" onClick={logoutUser}>Cerrar sesión</button>
+        <button className='item' onClick={logoutUser}>
+          Cerrar sesión
+        </button>
       </NavDropdown.Item>
     </NavDropdown>
   );
